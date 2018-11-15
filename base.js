31685b213b947e03e9822ab8134c499ad12666bf
Victor Markov <vikata3@gmail.com>
2018-11-16 01:17:18 +0200

    Add base scraper for one provider

--- base.js
+++ base.js
@@ -1,19 +1,41 @@
+const _ = require('lodash');
 const xray = require('x-ray');
-const x = Xray();
+const x = xray();
 
-x('https://videnov.bg/products/standartni-kuhni', {
-    items: x('.prod-teaser', [{
-        title: '.prod-teaser-title a',
-        image: '.prod-teaser-image img@src',
-        price: '.prod-teaser-price',
-        url: '.prod-teaser-title a@href',
-    }])
-})((err, result) => {
-    if (err) return;
-    console.log(result);
+const providers = [{
+    name: 'Videnov',
+    url: 'https://videnov.bg/',
+    baseUrl: 'https://videnov.bg/products',
+    products: [{
+        category: 'Kitchen',
+        url: 'standartni-kuhni',
+        schema: {
+            base: '.prod-teaser',
+            details: [{
+                title: '.prod-teaser-title a',
+                image: '.prod-teaser-image img@src',
+                price: '.prod-teaser-price',
+                url: '.prod-teaser-title a@href',
+            }]
+        }
+    }]
+}]
+
+_.each(providers, provider => {
+    _.each(provider.products, product => {
+        x(`${provider.baseUrl}/${product.url}`, {
+            items: x(product.schema.base, product.schema.details)
+        })((err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+            }
+        });
+    })
 });
 
 // persist data in mongo 
 // analyze images -- extract main colorss
 // invalidate data in mongo
-//
+// persist provider in data and allow it to be modifiable
